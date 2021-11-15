4c90f3dc08e8eb7c7729a6c7e3fe271b095967b4
amifidele <amifidele333@gmail.com>
2021-11-15 17:30:12 +0200

    creating user controller

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('age');
+            $table->integer('age');
             $table->string('location');
             $table->string('relationship');
             $table->timestamp('email_verified_at')->nullable();
