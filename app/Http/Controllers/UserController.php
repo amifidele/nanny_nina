612fc4802505304e97b503a837160f6451681652
amifidele <amifidele333@gmail.com>
2021-11-15 17:37:43 +0200

    add filter functionality in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+
+    public function filter(Request $request){
+        
+        $query= $request->input('query');
+
+
+        $users = User::where('name', 'like', '%$query%')
+                     ->orWhere('age', 'like', '%$query%')
+                     ->orWhere('location', 'like', '%$query%');
+
+        dd($users);
+
+
+    }
+
 }
